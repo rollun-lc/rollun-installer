325ef25086c86c13e307639eb2cf6b7b01dad616
victorynox <it.professor02@gmail.com>
2018-03-19 19:52:41 +0200

    fixed container init

--- config/container.php
+++ config/container.php
@@ -8,7 +8,9 @@ $config = require __DIR__ . '/config.php';
 
 // Build container
 $container = new ServiceManager();
-(new Config($config['dependencies']))->configureServiceManager($container);
+if(isset($config['dependencies'])) {
+    (new Config($config['dependencies']))->configureServiceManager($container);
+}
 
 // Inject config
 $container->setService('config', $config);
